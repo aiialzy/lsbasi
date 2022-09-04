9004903c1d4a548a976192ed294a506f5fb0c612
aiialzy <877090969@qq.com>
2022-09-04 10:21:18 +0800

    complete part4/part5

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -1,101 +1,56 @@
 package interpreter
 
 import (
+	"lsbasi/lexer"
 	"lsbasi/token"
 	"strconv"
-	"strings"
-	"unicode"
 )
 
 type Interpreter struct {
-	text         []rune
-	pos          int
 	currentToken *token.Token
-	currentChar  rune
+	lexer        *lexer.Lexer
 }
 
 func (i *Interpreter) error() {
 	panic("Invalid syntax")
 }
 
-func (i *Interpreter) integer() string {
-	var number strings.Builder
-	for unicode.IsDigit(i.currentChar) {
-		number.WriteRune(i.currentChar)
-		i.advance()
-	}
-
-	return number.String()
-}
-
-func (i *Interpreter) skipWhiteSpace() {
-	for unicode.IsSpace(i.currentChar) {
-		i.advance()
-	}
-}
-
-func (i *Interpreter) advance() {
-	i.pos += 1
-	if i.pos < len(i.text) {
-		i.currentChar = i.text[i.pos]
-	} else {
-		i.currentChar = 0
-	}
-}
-
-func (i *Interpreter) getNextToken() *token.Token {
-
-	if unicode.IsSpace(i.currentChar) {
-		i.skipWhiteSpace()
-	}
-
-	if i.currentChar == 0 {
-		return token.New(token.EOF, "")
-	}
-
-	if unicode.IsDigit(i.currentChar) {
-		number := i.integer()
-		return token.New(token.INTEGER, number)
-	}
-
-	switch i.currentChar {
-	case '+':
-		i.advance()
-		return token.New(token.PLUS, string(i.currentChar))
-
-	case '-':
-		i.advance()
-		return token.New(token.SUB, string(i.currentChar))
-
-	case '*':
-		i.advance()
-		return token.New(token.MUL, string(i.currentChar))
-
-	case '/':
-		i.advance()
-		return token.New(token.DIV, string(i.currentChar))
-
-	case '%':
-		i.advance()
-		return token.New(token.MOD, string(i.currentChar))
-	}
-
-	i.error()
-
-	return nil
-}
-
 func (i *Interpreter) eat(tokenType token.TokenType) {
 	if i.currentToken.Type == tokenType {
-		i.currentToken = i.getNextToken()
+		i.currentToken = i.lexer.GetNextToken()
 	} else {
 		i.error()
 	}
 }
 
-func (i *Interpreter) term() int64 {
+func (i *Interpreter) factor() int64 {
 	result, _ := strconv.ParseInt(i.currentToken.Value, 10, 64)
 	i.eat(token.INTEGER)
+	return result
+}
+
+func (i *Interpreter) term() int64 {
+	result := i.factor()
+
+	tokens := []token.TokenType{
+		token.MUL,
+		token.DIV,
+		token.MOD,
+	}
+
+	for i.in(i.currentToken.Type, tokens) {
+		if i.currentToken.Type == token.MUL {
+			i.eat(token.MUL)
+			result *= i.factor()
+		} else if i.currentToken.Type == token.DIV {
+			i.eat(token.DIV)
+			result /= i.factor()
+		} else if i.currentToken.Type == token.MOD {
+			i.eat(token.MOD)
+			result %= i.factor()
+		}
+	}
+
 	return result
 }
 
@@ -108,26 +63,12 @@ func (i *Interpreter) expr() int64 {
 	}
 
 	for i.in(i.currentToken.Type, tokens) {
-		switch i.currentToken.Type {
-		case token.PLUS:
+		if i.currentToken.Type == token.PLUS {
 			i.eat(token.PLUS)
 			result += i.term()
-
-		case token.SUB:
+		} else if i.currentToken.Type == token.SUB {
 			i.eat(token.SUB)
 			result -= i.term()
-
-		case token.MUL:
-			i.eat(token.MUL)
-			result *= i.term()
-
-		case token.DIV:
-			i.eat(token.DIV)
-			result /= i.term()
-
-		case token.MOD:
-			i.eat(token.MOD)
-			result %= i.term()
 		}
 	}
 	return result
@@ -144,19 +85,13 @@ func (i *Interpreter) in(tokenType token.TokenType, tokenTypes []token.TokenType
 }
 
 func (i *Interpreter) Interprete() int64 {
-	i.currentToken = i.getNextToken()
+	i.currentToken = i.lexer.GetNextToken()
 	return i.expr()
 }
 
 func New(text []rune) *Interpreter {
-	if len(text) == 0 {
-		panic("text length must bigger than 0")
-	}
-
 	return &Interpreter{
-		text:         text,
-		pos:          0,
 		currentToken: nil,
-		currentChar:  text[0],
+		lexer:        lexer.New(text),
 	}
 }
